d6cf148e88087f285771afb477ecee426c149f06
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-05-27 14:35:48 +0300

    Add about

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-//Express configuration
 const expressConfig = require("./config/expressConfig");
+const handlebarsConfig = require("./config/handlebarsConfig");
+const homeController = require("./controllers/homeController");
+
+//Express configuration
 expressConfig(app);
 //require('./config/expressConfig')(app);; //other syntax
 
 //Handlebars configuration
-const handlebarsConfig = require("./config/handlebarsConfig");
 handlebarsConfig(app);
 
-//Routes
-app.get("/", (req, res) => {
-    res.render('index');
-});
+app.use(homeController);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
