6927023f88a975cfd725c0b651a79cfe8097636c
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-05-28 15:13:00 +0300

    add 404 page

--- src/index.js
+++ src/index.js
@@ -17,5 +17,8 @@ handlebarsConfig(app);
 
 app.use(homeController);
 app.use("/cubes", cubeController); //only if the path starts with /cubes
+app.get("*", (req,res) => {
+    res.render("404")
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
